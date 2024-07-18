297b00c34dfaa1ff3322ceb9b474efa1f154a190
khush <khushjoshi007@gmail.c/usr/local/bin/python3 /Users/HCjoshi/Desktop/DSMP2.0/MLops/class5/pipeline/src/validation.pyom>
2024-07-18 13:46:37 +0530

    initialization of project

---
+++ mlflow/dataset.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import pathlib
+import yaml
+
+def get_data(path):
+    df = pd.read_csv(path/"iris.csv")
+    return df
+
+def split_data(params,df):
+    return train_test_split(df,test_size=params["split"])
+
+def save_data(train,test,save_path):
+    pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
+    train.to_csv(save_path / 'train.csv', index=False)
+    test.to_csv(save_path / 'test.csv', index=False)
+
+
+
+def main():
+
+    home_dir = pathlib.Path(__file__).parent.parent
+    data_path = home_dir / "data"/ "raw"
+    params_file = home_dir / "params.yaml"
+    save_path = home_dir / "data" / "processed"
+
+    params = yaml.safe_load(open(params_file))["dataset"]
+
+    df = get_data(data_path)
+    X,y = split_data(params,df)
+    save_data(X,y,save_path)
+
+if __name__ == "__main__":
+    main()
